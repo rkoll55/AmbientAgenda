dd32299d10f5bdb01e1ebbaba64b2b3e8a33a190
Rohan <rohankollambalath@gmail.com>
2023-09-03 10:34:29 +1000

    code for tkinter

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 import tkinter as tk
 from PIL import Image, ImageTk, ImageDraw, ImageFont
+import time
 
-def display_image(image_path):
-    root = tk.Tk()
-    root.title("Ambiance")
+root = tk.Tk()
+original_image = Image.open("template.png")
+panel = None
+label = None
 
-    original_image = Image.open(image_path)
-    
-    # Create a copy of the original image to add text
+def get_overlay_image(text_to_add):
     image_with_text = original_image.copy()
-    
-    text_to_add = "Soccer game at 2"
-    
-    # Create a drawing context for adding text to the image
     draw = ImageDraw.Draw(image_with_text)
     font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMonoBoldOblique.ttf", 36, encoding="unic")
-    draw.text((200, 200), text_to_add, fill="black", font=font)  
+    draw.text((100, 200), text_to_add, fill="black", font=font)  
+    return image_with_text
 
-    # Convert the modified image to a PhotoImage
+
+def display_image(image_with_text):
+    global root
+    global label
+
+    root.destroy(); root = tk.Tk()
     photo = ImageTk.PhotoImage(image_with_text)
-    
     label = tk.Label(root, image=photo)
     label.pack()
     label.photo = photo
-
     root.mainloop()
 
-# Example usage:
-image_path = "template.png"  # Replace with the path to your image
-display_image(image_path)
+def update_image(text_to_add):
+    overlay = get_overlay_image(text_to_add)
+    photo = ImageTk.PhotoImage(overlay)
+
+    label.config(image=photo)
+    label.image = photo
+
+
+
+display_image(get_overlay_image("Soccer game at 2"))
