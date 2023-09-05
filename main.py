be9f59ed64d736349a6b07c3d5a5547b32a4862f
Rohan <rohankollambalath@gmail.com>
2023-09-05 13:11:26 +1000

    finished overlay feature with JSON

--- main.py
+++ main.py
@@ -1,19 +1,114 @@
 import tkinter as tk
 from PIL import Image, ImageTk, ImageDraw, ImageFont
 import time
+import json
+import textwrap
 
 root = tk.Tk()
 original_image = Image.open("template.png")
 panel = None
 label = None
 
-def get_overlay_image(text_to_add):
+def read_json():
+
+    with open("overlay.json", "r") as json_file:
+        json_data = json.load(json_file)
+
+    return json_data
+
+
+def get_overlay_image():
     image_with_text = original_image.copy()
     draw = ImageDraw.Draw(image_with_text)
     font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMonoBoldOblique.ttf", 36, encoding="unic")
-    draw.text((100, 200), text_to_add, fill="black", font=font)  
+    js = read_json()
+
+    for day, day_data in js.items():
+        if day == "Monday":
+            offset = 150
+            if isinstance(day_data, dict):
+                for user, events in day_data.items():
+                    colour = "blue" if user == "user1" else "black"
+                    for event in events:
+                        wrapped_text = textwrap.fill(event, width=15)
+                        draw.multiline_text((100, offset), wrapped_text, fill=colour, font=font)  
+                        offset+= 50
+                        offset += len(wrapped_text.split('\n')) * 25
+
+        elif day == "Tuesday":
+            offset = 150
+            if isinstance(day_data, dict):
+                for user, events in day_data.items():
+                    colour = "blue" if user == "user1" else "black"
+                    for event in events:
+                        wrapped_text = textwrap.fill(event, width=15)
+                        draw.multiline_text((450, offset), wrapped_text, fill=colour, font=font)  
+                        offset+= 50
+                        offset += len(wrapped_text.split('\n')) * 25
+        
+        elif day == "Wednesday":
+            offset = 150
+            if isinstance(day_data, dict):
+                for user, events in day_data.items():
+                    colour = "blue" if user == "user1" else "black"
+                    for event in events:
+                        wrapped_text = textwrap.fill(event, width=15)
+                        draw.multiline_text((810, offset), wrapped_text, fill=colour, font=font)  
+                        offset+= 50
+                        offset += len(wrapped_text.split('\n')) * 25
+        
+        elif day == "Thursday":
+            offset = 150
+            if isinstance(day_data, dict):
+                for user, events in day_data.items():
+                    colour = "blue" if user == "user1" else "black"
+                    for event in events:
+                        wrapped_text = textwrap.fill(event, width=15)
+                        draw.multiline_text((1175, offset), wrapped_text, fill=colour, font=font)  
+                        offset+= 50
+                        offset += len(wrapped_text.split('\n')) * 25
+
+
+        elif day == "Friday":
+            offset = 150
+            if isinstance(day_data, dict):
+                for user, events in day_data.items():
+                    colour = "blue" if user == "user1" else "black"
+                    for event in events:
+                        wrapped_text = textwrap.fill(event, width=15)
+                        draw.multiline_text((1530, offset), wrapped_text, fill=colour, font=font)  
+                        offset+= 50
+                        offset += len(wrapped_text.split('\n')) * 25
+
+
+        elif day == "Saturday":
+            offset = 750
+            if isinstance(day_data, dict):
+                for user, events in day_data.items():
+                    colour = "blue" if user == "user1" else "black"
+                    for event in events:
+                        wrapped_text = textwrap.fill(event, width=40)
+                        draw.multiline_text((90, offset), wrapped_text, fill=colour, font=font)  
+                        offset+= 50
+                        offset += len(wrapped_text.split('\n')) * 25
+
+
+        elif day == "Sunday":
+            offset = 750
+            if isinstance(day_data, dict):
+                for user, events in day_data.items():
+                    colour = "blue" if user == "user1" else "black"
+                    for event in events:
+                        wrapped_text = textwrap.fill(event, width=40)
+                        draw.multiline_text((990, offset), wrapped_text, fill=colour, font=font)  
+                        offset+= 50
+                        offset += len(wrapped_text.split('\n')) * 25
+
+    
+
     return image_with_text
 
+read_json()
 
 def display_image(image_with_text):
     global root
@@ -35,4 +130,4 @@ def update_image(text_to_add):
 
 
 
-display_image(get_overlay_image("Soccer game at 2"))
+display_image(get_overlay_image())
