f93648623edd351c7d6d75525aace1c347f766f2
Gil Wise <gilwise245@gmail.com>
2023-09-13 22:29:43 +1000

    Gil's observer

---
+++ src/gil_obersver.py
@@ -0,0 +1,53 @@
+# Observer Script
+import RPi as GPIO #Need to install "Microsoft C++ Build Tools first"
+import subprocess
+from picamera import PiCamera
+import time
+import toby_ML
+
+GPIO.setmode(GPIO.BCM)
+
+LIDR_PIN = 1
+PHOTO_BUTTON_PIN = 2
+TRIGGER_READING = 1500
+
+GPIO.setup(LIDR_PIN, GPIO.IN)
+GPIO.setup(PHOTO_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+camera = PiCamera()
+
+displayer_process = None
+button_pressed = False
+
+try:
+    while True:
+        light_reading = GPIO.input(LIDR_PIN)
+        if light_reading >= TRIGGER_READING and displayer_process is None:
+            displayer_process = subprocess.Popen(["python3", "displayer_program.py"])
+            break
+        
+    camera.start_preview()
+    time.sleep(3)
+    while True:
+        
+        button_state = GPIO.input(PHOTO_BUTTON_PIN)
+        '''
+        if light_reading >= TRIGGER_READING and displayer_process is None:
+            displayer_process = subprocess.Popen(["python3", "displayer_program.py"])
+        elif light_reading < TRIGGER_READING and displayer_process is not None:
+            displayer_process.terminate()
+            displayer_process = None
+        '''
+        if button_state == GPIO.LOW and not button_pressed:
+            camera.capture('capture.jpg')
+            json = toby_ML.doMLStuff('capture.jpg')
+            # push to cloud over here
+            button_pressed = True
+        elif button_state == GPIO.HIGH:
+            button_pressed = False
+
+except KeyboardInterrupt:
+    if displayer_process is not None:
+        displayer_process.terminate()
+    camera.stop_preview()
+    GPIO.cleanup()
