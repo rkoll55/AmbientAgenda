21b96b2ec2abf5b83181cc30286e96d48453ec5f
Rohan <rohankollambalath@gmail.com>
2023-09-17 14:49:51 +1000

    added sound when keyword is present in event

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ from PIL import Image, ImageTk, ImageDraw, ImageFont
 import time
 import json
 import textwrap
-
-
-import asyncio
-import aiohttp
+import os
+from playsound import playsound
 import threading 
 
 
 original_image = Image.open("images/template.png")
 panel = None
 label = None
+sounds_made = [False, False, False, False, False]
+
+
+def play_sound(type="base"):
+    playsound("chime.mp3") 
+
 
 def read_json():
 
@@ -20,8 +24,13 @@ def read_json():
         json_data = json.load(json_file)
     return json_data
 
+def make_sound(event):
+    if sounds_made[0] == False and (event.find("dinner") or event.find("lunch") or event.find("breakfast")):
+        sound_thread = threading.Thread(target=play_sound); sound_thread.start()
+        sounds_made[0] = True
 
 def get_overlay_image():
+    sound_played = False
     image_with_text = original_image.copy()
     draw = ImageDraw.Draw(image_with_text)
     font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMonoBoldOblique.ttf", 36, encoding="unic")
@@ -34,6 +43,7 @@ def get_overlay_image():
                 for user, events in day_data.items():
                     colour = "blue" if user == "user1" else "black"
                     for event in events:
+                        sound_played = make_sound(event) if not sound_played else sound_played
                         wrapped_text = textwrap.fill(event, width=15)
                         draw.multiline_text((100, offset), wrapped_text, fill=colour, font=font)  
                         offset+= 50
@@ -45,6 +55,7 @@ def get_overlay_image():
                 for user, events in day_data.items():
                     colour = "blue" if user == "user1" else "black"
                     for event in events:
+                        sound_played = make_sound(event) if not sound_played else sound_played
                         wrapped_text = textwrap.fill(event, width=15)
                         draw.multiline_text((450, offset), wrapped_text, fill=colour, font=font)  
                         offset+= 50
@@ -56,6 +67,7 @@ def get_overlay_image():
                 for user, events in day_data.items():
                     colour = "blue" if user == "user1" else "black"
                     for event in events:
+                        sound_played = make_sound(event) if not sound_played else sound_played
                         wrapped_text = textwrap.fill(event, width=15)
                         draw.multiline_text((810, offset), wrapped_text, fill=colour, font=font)  
                         offset+= 50
@@ -67,6 +79,7 @@ def get_overlay_image():
                 for user, events in day_data.items():
                     colour = "blue" if user == "user1" else "black"
                     for event in events:
+                        sound_played = make_sound(event) if not sound_played else sound_played
                         wrapped_text = textwrap.fill(event, width=15)
                         draw.multiline_text((1175, offset), wrapped_text, fill=colour, font=font)  
                         offset+= 50
@@ -79,6 +92,8 @@ def get_overlay_image():
                 for user, events in day_data.items():
                     colour = "blue" if user == "user1" else "black"
                     for event in events:
+                        sound_played = make_sound(event) if not sound_played else sound_played
+
                         wrapped_text = textwrap.fill(event, width=15)
                         draw.multiline_text((1530, offset), wrapped_text, fill=colour, font=font)  
                         offset+= 50
@@ -91,6 +106,8 @@ def get_overlay_image():
                 for user, events in day_data.items():
                     colour = "blue" if user == "user1" else "black"
                     for event in events:
+                        sound_played = make_sound(event) if not sound_played else sound_played
+
                         wrapped_text = textwrap.fill(event, width=40)
                         draw.multiline_text((90, offset), wrapped_text, fill=colour, font=font)  
                         offset+= 50
@@ -103,6 +120,8 @@ def get_overlay_image():
                 for user, events in day_data.items():
                     colour = "blue" if user == "user1" else "black"
                     for event in events:
+                        sound_played = make_sound(event) if not sound_played else sound_played
+
                         wrapped_text = textwrap.fill(event, width=40)
                         draw.multiline_text((990, offset), wrapped_text, fill=colour, font=font)  
                         offset+= 50
@@ -131,7 +150,9 @@ def update_image(text_to_add):
 
 def main_thread():
     while True:
+
         # Displaying initial state of GUI
+        
         display_image(get_overlay_image())
         # Running tkinter main loop
 
