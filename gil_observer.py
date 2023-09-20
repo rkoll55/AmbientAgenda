64ccbe6608ee6b34f759854ba6fc1fabee248f28
MatildaDamman <dammanmatilda@gmail.com>
2023-09-20 16:42:50 +1000

    added aiden code

--- gil_observer.py
+++ gil_observer.py
@@ -4,8 +4,15 @@ import subprocess
 from picamera import PiCamera
 import time
 import recog
+from azure.iot.device import IoTHubDeviceClient, Message
+import json
 
+CONNECTION_STRING = "HostName=deco3801.azure-devices.net;DeviceId=raspberrypi1;SharedAccessKey=jZKYxNL3AY5EuMBZF/p9YDUy54ClsFYStjhp/v8E4yg="
 GPIO.setmode(GPIO.BCM)
+
+def iothub_client_init():
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+    return client
 
 LIDR_PIN = 1
 PHOTO_BUTTON_PIN = 2
@@ -40,8 +47,22 @@ try:
         '''
         if button_state == GPIO.LOW and not button_pressed:
             camera.capture('capture.jpg')
-            json = recog.write_to_temp(recog.box_recog('capture.jpg'), infile="template.json", outfile="output.json") #has default file paths
+            json_object = recog.write_to_temp(recog.box_recog('capture.jpg'), infile="template.json", outfile="output.json") #has default file paths
             # push to cloud over here
+            print("we running")
+            client = iothub_client_init()
+            #client.connect()
+
+
+            #the code to send
+            print(json_object)
+            print(str(json_object))
+            message = Message(str(json_object))
+            message.content_encoding = "utf-8"
+            message.content_type = "application/json"
+            client.send_message(message)
+            #no idea if it works
+
             button_pressed = True
         elif button_state == GPIO.HIGH:
             button_pressed = False
